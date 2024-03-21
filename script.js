a631f4c928619604e79d9dd4ced7ecee612a596f
Isidoro <marcelo.idantonio@senacsp.edu.br>
2024-03-20 21:05:57 -0300

    Terminado o JS de escolhas do pokes

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const pokemonsCard = document.querySelectorAll('.cartao-pokemon');
 const listaSelecaoPokemons = document.querySelectorAll('.pokemon');
 
+listaSelecaoPokemons.forEach(pokemon => {
+
+    pokemon.addEventListener('click', () => {
+        
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        //Remove a class aberto desta tag para que as informações dele sejam ocultadas
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+        // concatenando o id do pokemon com o prefixo cartao-
+        const cartaoPokemonParaAbrir  = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        pokemon.classList.add('ativo')
+
+    })
+})
+
 //selecionando a tag com a class .imagemLOGO
 let logo = document.querySelector('.imagemLOGO')
 //selecionando a tag com a class .imagemA
@@ -33,3 +55,67 @@ pikachu.addEventListener('click', () => {
     choque.style.display = 'block'
     choque.play()
 })
+
+let folha = document.querySelector('.folha');
+let bulbasauro = document.querySelector('.imagemB')
+bulbasauro.addEventListener('click', () => {
+    folha.style.display = 'block'
+    folha.play()
+})
+
+let fogo = document.querySelector('.fogo')
+let charmander = document.querySelector('.imagemC')
+charmander.addEventListener('click', () => {
+    fogo.style.display = 'block'
+    fogo.play()
+})
+
+let agua = document.querySelector('.agua')
+let gyarados = document.querySelector('.imagemD')
+gyarados.addEventListener('click', () => {
+    agua.style.display = 'block'
+    agua.play()
+})
+
+let fanta = document.querySelector('.fantasma')
+let gengar = document.querySelector('.imagemE')
+gengar.addEventListener('click', () => {
+    fanta.style.display = 'block'
+    fanta.play()
+})
+
+let drago = document.querySelector('.dragao')
+let dragonite = document.querySelector('.imagemF')
+dragonite.addEventListener('click', () => {
+    drago.style.display = 'block'
+    drago.play()
+})
+
+let aviso = document.querySelector('.aviso')
+
+function mouseEntrar() {
+    pikachu.style.transform = 'scale(1.2)'
+    bulbasauro.style.transform = 'scale(1.3)'
+    charmander.style.transform = 'scale(1.1)'
+    gyarados.style.transform = 'scale(1.1)'
+    gengar.style.transform = 'scale(1.2)'
+    dragonite.style.transform = 'scale(1.1)'
+    logo.style.transform = 'scale(1.2)'
+    //alterando a opacidadedo aviso para 1
+    aviso.style.opacity = '1'
+    //alterando a escala do aviso
+    aviso.style.transform = 'scale(1)'
+}
+
+function mouseSair() {
+    pikachu.style.transform = 'scale(1)'
+    bulbasauro.style.transform = 'scale(1)'
+    charmander.style.transform = 'scale(1)'
+    gyarados.style.transform = 'scale(1)'
+    gengar.style.transform = 'scale(1)'
+    dragonite.style.transform = 'scale(1)'
+    logo.style.transform = 'scale(1.1)'
+    aviso.style.opacity = '0'
+    aviso.style.transform = 'scale(.8)'
+}
+
